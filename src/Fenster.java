7461b0f62ce7d41b59bf8405c2335bbc43db4b6d
Sophia Breithaupt <sophia.Breithaupt@schiller-og.de>
2023-07-10 00:47:23 +0200

    Hintergrund

--- src/Fenster.java
+++ src/Fenster.java
@@ -13,19 +13,26 @@ public class Fenster implements ActionListener{
 				f = new JFrame();
 				f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 f.setSize(800,600);
-				label= new JLabel("<html><body>Liebe/r zukünftige/r Schillerschüler/in,<br><br>herzlich willkommen am Schiller-Gymnasium! Wir freuen uns sehr, euch als neue Mitglieder unserer Schulgemeinschaft <br>begrüßen zu dürfen. Mit diesem Brief möchten wir euch einen herzlichen Empfang bereiten und<br> euch einen ersten Eindruck von unserem Gymnasium vermitteln. Wir sind uns sicher, dass ihr euren ersten Schultag genießen werdet. Wir wünschen euch viel Spaß mit neuen Freund/innen und viel Erfolg beim lernen.<br> Mit vielen lieben Grüßen,<br><br>Euer Schiller-Gymnasium </body></html>");
-            
-				panel1 = new JPanel();
 				
+				panel1 = new JPanel();
+				panel1.setLayout(null);
+
+
+				label= new JLabel("<html><body>Liebe/r zukünftige/r Schillerschüler/in,<br><br>herzlich willkommen am Schiller-Gymnasium! <br>Wir freuen uns sehr, euch als neue Mitglieder unserer Schulgemeinschaft <br>begrüßen zu dürfen. Mit diesem Brief möchten wir euch einen herzlichen Empfang bereiten<br> und euch einen ersten Eindruck von unserem Gymnasium vermitteln.<br> Wir sind uns sicher, dass ihr euren ersten Schultag genießen werdet.<br> Wir wünschen euch viel Spaß mit neuen Freund/innen und viel Erfolg beim lernen.<br> Mit vielen lieben Grüßen,<br><br>Euer Schiller-Gymnasium </body></html>");
+				label.setBounds(50, 50, 700, 400); 
+				label.setHorizontalAlignment(SwingConstants.CENTER);
+
                 panel1.add(label);
+				JLabel backgroundLabel = Hintergrund();
+     			panel1.add(backgroundLabel);
+				
 
 				button = new JButton ("weiter");
 				button.addActionListener(this);
+				button.setBounds(350, 500, 100, 30);
                 panel1.add(button);
-                f.add(panel1);
-                
 
-				f.setSize(1300,900);
+                f.add(panel1);
 				f.setVisible(true);
 
 			}
@@ -38,6 +45,13 @@ public class Fenster implements ActionListener{
 			f.repaint();
 		}
 	}
+	public JLabel Hintergrund (){
+		ImageIcon backgroundImage = new ImageIcon("hintergrundbild.jpg");
+		JLabel backgroundLabel = new JLabel(backgroundImage);
+		backgroundLabel.setBounds(0, 0, backgroundImage.getIconWidth(), backgroundImage.getIconHeight());
+		return backgroundLabel;
+	}
+		
 	public static void main(String[] args){
 		Fenster fenster = new Fenster();
 		fenster.Bild();
